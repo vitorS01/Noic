6b87921d595d2053c62274ccaec975773a9c3d22
Márcio Vitor <90209124+vitorS01@users.noreply.github.com>
2025-07-13 19:40:13 -0300

    Update Grafico_de_Barras.cpp

--- Grafico_de_Barras.cpp
+++ Grafico_de_Barras.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main(){
     int n, H;
     cin >> n;
-    vector<int> altura(n);
+    vector<int> a(n);
 
-    for (int &v : altura) cin >> v;
+    for (int &v : a) cin >> v;
 
-    H = *max_element(altura.begin(), altura.end()); // Pega o maior elemento do vetor
+    H = *max_element(a.begin(), a.end()); // Pega o maior elemento do vetor
     vector<vector<int>> m(H, vector<int>(n, 0)); // Inicializa com 0's
 
     for(int j = 0;j < n; j++) {
-        for (int i = H-altura[j]; i < H; i++) {
+        for (int i = H-a[j]; i < H; i++) {
             m[i][j] = 1;
         }
     }
