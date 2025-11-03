69fc070deba466664bc6c99100fe72724afd167d
Márcio Vitor <90209124+vitorS01@users.noreply.github.com>
2025-11-03 19:17:10 -0300

    Enhance comments for subarray analysis algorithm
    Added detailed comments explaining the algorithm for counting subarrays and their contributions based on maximum values.

--- OBOI_2025/Hoteis_Infinitos.cpp
+++ OBOI_2025/Hoteis_Infinitos.cpp
@@ -10,6 +10,34 @@ const int INV2 = 500000004;
 const int MOD = 1e9 + 7;
 int a[MAXN],lf[MAXN],rg[MAXN];
 
+/*
+Vamos analisar uma subarray definida por [L,R] , seja x1,x2,y1,y2 as cordenadas que definem a submatriz. x1<=x2, y1<=y2
+(sendo M[x1][y1] o canto superior esquerdo da foto e M[x2][y2] o canto superior direito da foto).
+Seja M = max(a[L], a[L+1],...a[R]). Temos então que x1=L, x2=R. Vamos contar agora a quantidade de y1<=y2 satisfazem a condição 
+para ser uma foto, primeiro observe que é necessário e suficiente que y2 <= M. pois caso contrário a foto ficaria mostraria o fim do maior hotel. 
+e se for verdade com certeza temos uma foto em que pelo menos o maior hotel parece infinito. 
+Ou seja basta contar quantos 1<=y1<=y2 <= M. Isso é (M+1 escolhe 2). Agora basta calcular M+1 escolhe 2 para todo [L,R].
+
+Perceba que podemos calcular a contribuição de cada elemento, A[i]. 
+Ou seja para cada elemento contar em quantas subarrays esse é o máximo mais a esquerda e então adicionar a resposta A[i]*(A[i]+1)/2*(contribuição)
+á resposta. Para calcular a contribuição basta achar o j mais próximo a esquerda tal que A[i] <= A[j] 
+e o k mais próximo a direita tal que A[i] < a[k]. então a contribuição é (i-j)*(k-i). 
+Para calcular j e k só usar uma stack monotônica e dois passes na array. Resolvendo o problema em O(N)
+
+explicação das parciais:
+- Subtarefa 1: 
+podemos só brutar todos os x1,x2,y1,y2 e verificar se a condição é satisfeita em O(N) iterando pela linha do topo
+e vendo se ela tem pelo menos um andar de um prédio, complexidade final O(n^5)
+- Subtarefa 2:
+da pra otimizar a subtarefa 1, usando soma de prefixo para uma complexidade em O(N^4)
+- Subtarefa 3:
+Para essa subtarefa podemos brutar todos L,R e adicionar o máximo desse range escolhe 2
+- Subtarefa 4:
+Nesse caso é bem mais fácil calcular as contribuições já que i é o máximo de todos os intervalos com L<=i e R=i.
+Assim é só iterar pela array e adicionar i*(a[i]+1)*a[i]/2 a resposta.
+
+*/
+
 int32_t main() {
 	ios_base::sync_with_stdio(0); cin.tie(nullptr);
 	int n;cin >> n;
